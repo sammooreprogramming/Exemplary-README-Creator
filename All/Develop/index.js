cffe1adce2e5d5f7be1a74008bc674f166abe6bd
Sam Moore <sammooresemail23@gmail.com>
2022-07-24 16:51:35 -0700

    created an MVP to write a readme file

--- All/Develop/index.js
+++ All/Develop/index.js
@@ -2,8 +2,10 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+
 // TODO: Create an array of questions for user input
-const questions = [
+ questions = () => {
+    inquirer.prompt([
     {
     type: "input",
     name: "title",
@@ -45,13 +47,21 @@ const questions = [
     name: "repository",
     message: "What is your Github repository link?"
     },
-];
+]).then(console.log(answers))};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeReadMe(fileName, data) {
+    fs.writeFile("README.md", data, err => {
+        if (err) {
+            throw err;
+        } else console.log("Your shiny new file has been created!")
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeReadMe("README.md", "test")
+}
 
 // Function call to initialize app
 init();
