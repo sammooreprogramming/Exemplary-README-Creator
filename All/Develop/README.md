0a33de3aee9adedfa24ecd907c17e85121f0fba0
Sam Moore <sammooresemail23@gmail.com>
2022-07-27 17:19:25 -0700

    added renderLicenseBadge function

--- All/Develop/README.md
+++ All/Develop/README.md
@@ -1,34 +1,48 @@
-# Exemplary README.md Creator
+# a
+
+  ## Table of Contents
+  # Description(#description)
+  # Installation(#description)
+  # Usage(#usage)
+  # Credits(#credits)
+  # License(#license)
+  # Github Information(#github)
 
 
 
 # Description
 
-This application is designed to custom-tailor a README.md file based on the user's input to several questions that corelate to a section in the README.md file. It was designed to ease the process of writing tedious files that are also very important to have.
+a
+
 
 
 # Installation
 
-To run the application: You must install the dependencies, which are as follows: 1. Please clone the GitHub repository. 2. install inquirer and node js also.
+To run the application: a
+
 
 
 # Usage
 
-This application is used thusly: By asking the user a series of questions using inquirer and node.js the application compiles that data and arranges a README.md file like magic.
+This application is used thusly: a
+
 
 
 # Credits
 
-The following contributed to this assignment: I, Sam Moore,  wrote this program.
+The following contributed to this assignment: a
+
 
 
 # License
 
+
 This application's license category is: 
+
 
 
 # GitHub Information
 
-Here is the link to the GitHub page: https://github.com/sammooreprogramming/Exemplary-README-Creator
+Here is the link to the GitHub page: a
 
 #
--- All/Develop/utils/generateMarkdown.js
+++ All/Develop/utils/generateMarkdown.js
@@ -1,7 +1,27 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+ if (license === MIT)   if (license === MIT) {
+  return '[![License: MIT](https://img.shields.io/badge/node-lightblue?logo=node&style=plastic)](https://opensource.org/licenses/MIT)';
+ }
 
+ else if (license === Mozilla) {
+  return '[![License: Mozilla Public License 2.0](https://img.shields.io/badge/Mozilla-grey?logo=node&style=plastic)](https://opensource.org/licenses/MPL-2.0)';
+ }
+
+ else if (license === ISC) {
+  return '[![License: ISC](https://img.shields.io/badge/ISC-orange?logo=node&style=plastic)](https://opensource.org/licenses/ISC)';
+ }
+
+ else if (license === GNU) {
+  return '[![License: General Public License](https://img.shields.io/badge/GNU-red?logo=node&style=plastic)](https://opensource.org/licenses/GPL-3.0)';
+ }
+ 
+ else if (license === Apache) {
+  return '[![License: Apache 2.0](https://img.shields.io/badge/Apache%202.0-green?logo=node&style=plastic)](https://opensource.org/licenses/Apache-2.0)';
+ }
+
+ else return "";
 }
 
 // TODO: Create a function that returns the license link
@@ -13,7 +33,7 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  
+
 }
 
 // TODO: Create a function to generate markdown for README
@@ -34,9 +54,11 @@ function generateMarkdown(data) {
 ${data.description}
 
 
+
 # Installation
 
 To run the application: ${data.installation}
+
 
 
 # Usage
@@ -44,15 +66,18 @@ To run the application: ${data.installation}
 This application is used thusly: ${data.usage}
 
 
+
 # Credits
 
 The following contributed to this assignment: ${data.credits}
+
 
 
 # License
 
 
 This application's license category is: ${data.license}
+
 
 
 # GitHub Information
